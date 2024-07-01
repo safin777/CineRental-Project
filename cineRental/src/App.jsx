44cee1641198f8eea2108f3103fc67c9a4c29720
safin777 <sayedislamsafin7770555@gmail.com>
2024-07-01 20:59:26 +0600

    Project with Reducer in React with Toastify

--- cineRental/src/App.jsx
+++ cineRental/src/App.jsx
@@ -1,18 +1,23 @@
 //import css
 import './App.css'
 import './index.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { MovieContext, ThemeContext } from './context/index'
-import { useState } from 'react'
+import { useState,useReducer } from 'react'
 import Page from './Page'
+import { CartReducer,initialState } from './reducer/CartReducer'
 function App() {
-  const [cartData, setCartData] = useState([]) //initialize the cartData state with an empty array
+  //const [cartData, setCartData] = useState([]) //initialize the cartData state with an empty array
   const [darkMode,setDarkMode]=useState(true)
+  const [state, dispatch] = useReducer(CartReducer, initialState)
   return (
     <>
       <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-        <MovieContext.Provider value={{ cartData, setCartData }}>
+        <MovieContext.Provider value={{ state, dispatch }}>
           <Page />
+          <ToastContainer />
         </MovieContext.Provider>
       </ThemeContext.Provider>
     </>
